c96f87a89445d0920015fdcdc43355076fca6e14
Pranith Macha <pranith@machaiswho.im>
2017-02-19 21:01:45 -0600

    importing local settings

--- university/university/settings.py
+++ university/university/settings.py
@@ -100,3 +100,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from university.university import localsettings
+except ImportError:
+    print("could not find localsettings")
