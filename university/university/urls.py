08d461546093be87871030248fe65ce488b8071e
Pranith Macha <pranith@machaiswho.im>
2017-03-04 21:30:55 -0600

    static setings

--- university/university/urls.py
+++ university/university/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+# from django.views.generic import TemplateView
+from university import views
+
+# url(r'^user/', include('django.contrib.auth.urls')),
+
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'university.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.home, name='home'),
+    url(r'^user/login/$', auth_views.login,
+        {'template_name': 'login.html'}, name='login'),
+    url(r'^user/logout/$', auth_views.logout, {'template_name': 'logout.html'}, name='logout'),
 ]
